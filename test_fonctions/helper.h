17333f81d0471013674c17a7785ae4f7ed257a4c
Adam <adam.rili@epita.fr>
2022-10-27 10:01:55 +0200

    changement des structures. Trouver les nouvelle dans le .h de test_fonctions

--- test_fonctions/helper.h
+++ test_fonctions/helper.h
@@ -1,100 +1,158 @@
-//
-// Created by debor on 17/10/2022.
-//
-
 #ifndef PROJET_L2_DICO_NODE_H
 #define PROJET_L2_DICO_NODE_H
 
-typedef struct TYPE
-{
-    int masc;
-    int fem;
-    int sing;
-    int plu;
-    int inv;
-    int base;
-}Type;
 
-typedef struct VTYPE
-{
-    struct TYPE type;
-    int p1;
-    int p2;
-    int p3;
-    char *temp;
-    /*
-    ppre; //participe
-    spre; // subjonctif
-    ipre; //indicatif
-    cpre; //conditionnel
-    iimp; //imparfait de l indicatif
-    simp; //imparfait du subjonctif
-    ifut; //futur
-    ipsim; //passe simple
-    inf; //infinitif
-    ppas; //participe passe
-    impre; //imperatif
-    */
-}Vtype;
 
-typedef struct VERBE
-{
-    char lettre;
-    struct VTYPE type;
-    int nb_flechie; //nombre de declinaisons du verbe
-    int end; //si la lettre complete un verbe
-}Vb;
 
-typedef struct NOM
-{
-    char lettre;
-    struct TYPE type;
-    int nb_flechie; //nombre de declinaisons du verbe
-    int end; //si la lettre complete un verbe
-}Nom;
-
-typedef struct ADV
-{
-    char lettre;
-    int nb_flechie; //=1 //nombre de declinaisons du verbe
-    int end; //si la lettre complete un verbe
-}Adv;
-
-typedef struct ADJ
-{
-    char lettre;
-    struct TYPE type;
-    int nb_flechie; //nombre de declinaisons de l adj
-    int end; //si la lettre complete un verbe
-}Adj;
-
-typedef struct NODE_VERBE
-{
-    struct VERBE *vb;
-    int nbchildren; //nombre de lettres suivant celle on l on se trouve
-    struct NODE_VERBE *children[]; //tab dyn des adresses des enfants
-}Nvb;
-
-typedef struct NODE_NOM
-{
-    struct NOM *nom;
-    int nbchildren; //nombre de lettres suivant celle on l on se trouve
-    struct NODE_NOM *children[]; //tab dyn des adresses des enfants
-}Nnom;
-
+/*
+=====================================================================================
+                            Structure for ADV
+=====================================================================================
+*/
+//pour stocker les formes de base dans une liste chainee
 typedef struct NODE_ADV
 {
-    struct ADV *adv;
-    int nbchildren; //nombre de lettres suivant celle on l on se trouve
-    struct NODE_ADV *children[]; //tab dyn des adresses des enfants
-}Nadv;
+    char *mot;
+    NODE_ADJ *next;
+}
 
+//pour stocker le nombre de forme de base
+//ainsi qu'un pointeur vers la liste chainee des formes de base
+typedef struct ADV
+{
+    int nb_fdb;
+    NODE_ADJ *base;
+}
+
+
+
+
+/*
+=====================================================================================
+                            Structure for ADJ
+=====================================================================================
+*/
+//pour stocker les formes flechits dans une liste chainee
+//ainsi que les parametres de cette forme
+typedef struct FF_ADJ
+{
+    char *ff;
+    int diff;
+    char *genre;
+    char *nombre;    
+    FF_ADJ *next;
+};
+
+//pour stocker les formes de base dans une liste chainee
+//avec un pointeur vers les formes flechits ainsi que leur nombre
 typedef struct NODE_ADJ
 {
-    struct ADJ *adj;
-    int nbchildren; //nombre de lettres suivant celle on l on se trouve
-    struct NODE_ADJ *children[]; //tab dyn des adresses des enfants
-}Nadj;
+    char *mot;
+    int nb_flechie;
+    FF_ADJ *ff;
+    NODE_ADJ *next;
+}
+
+//pour stocker le nombre de forme de base
+//ainsi qu'un pointeur vers la liste chainee des formes de base
+typedef struct ADJ
+{
+    int nb_fdb;
+    NODE_ADJ *base;
+}
 
 
-#endif //PROJET_L2_DICO_NODE_H
+
+
+/*
+=====================================================================================
+                            Structure for NOM
+=====================================================================================
+*/
+//pour stocker les formes flechits dans une liste chainee
+//ainsi que les parametres de cette forme
+typedef struct FF_NOM
+{
+    char *ff;
+    int diff;
+    char *genre;
+    char *nombre;
+    FF_ADJ *next;
+};
+
+//pour stocker les formes de base dans une liste chainee
+//avec un pointeur vers les formes flechits ainsi que leur nombre
+typedef struct NODE_NOM
+{
+    char *mot;
+    int nb_flechie;
+    FF_ADJ *ff;
+    NODE_ADJ *next;
+}
+
+//pour stocker le nombre de forme de base
+//ainsi qu'un pointeur vers la liste chainee des formes de base
+typedef struct NOM
+{
+    int nb_fdb;
+    NODE_ADJ *base;
+}
+
+
+
+
+/*
+===============================================================================
+                        STRUCTURE POUR LES VERBES
+===============================================================================
+*/
+//pour stocker les formes flechits dans une liste chainee
+//ainsi que les parametres de cette forme
+typedef struct FF_VB
+{
+    char *ff;
+    int diff;
+    char *personne;
+    char *conjugaison;
+    char *nombre;
+    char *genre;
+    FF_ADJ *next;
+};
+
+//pour stocker les formes de base dans une liste chainee
+//avec un pointeur vers les formes flechits ainsi que leur nombre
+typedef struct NODE_VB
+{
+    char *mot;
+    int nb_flechie;
+    FF_ADJ *ff;
+    NODE_ADJ *next;
+}
+
+//pour stocker le nombre de forme de base
+//ainsi qu'un pointeur vers la liste chainee des formes de base
+typedef struct VB
+{
+    int nb_fdb;
+    NODE_ADJ *base;
+}
+
+
+
+
+/*
+===============================================================================
+                        STRUCTURE POUR L'ABRE
+Cette arbre contient les quatres structure pour les verbes, adj, adv et nom
+===============================================================================
+*/
+
+typedef struct TREE
+{
+    VB *verbes;
+    ADJ *adjectifs;
+    ADV *adverbes;
+    NOM *noms;
+};
+
+#endif

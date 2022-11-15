c6e0ce7bf7d8f31e3a96b8e6e58c910015284e3d
iTiMaX <106245562+iTiMaX@users.noreply.github.com>
2022-11-15 22:30:45 +0100

    upgrade menu

--- Projet_L2/main.c
+++ Projet_L2/main.c
@@ -3,6 +3,8 @@
 #include "../test_fonctions/nom.h"
 #include "../test_fonctions/adj.h"
 #include "../test_fonctions/adv.h"
+#include "stdio.h"
+#include "stdlib.h"
 
 
 int main() {
@@ -30,6 +32,9 @@ int main() {
     dico_read("../Projet_L2/TreeExample/dictionnaire.txt", &vb, &nom, &adj, &adv);
 
     int choix = 0;
+    int choix1 = 0;
+    int choix2 = 0;
+    char *res;
     printf("Bonjour, bienvenue sur notre generateur automatique de phrases\n");
     printf("Vous pourrez choisir entre la generation ou la recherche de mot\n");
     printf("dans le dictionnaire.\n");
@@ -41,57 +46,124 @@ int main() {
 
         switch (choix) {
             case 1: {
-                printf("phrase 1: ");
-                __Nom_Aleatoire2(nom.child[rand() % nom.nbenfant]);
-                printf(" ");
-                __Adj_Aleatoire2(adj.child[rand() % adj.nbenfant]);
-                printf(" ");
-                __Verbe_Aleatoire2(vb.child[rand() % vb.nbenfant]);
-                printf(" ");
-                __Nom_Aleatoire2(nom.child[rand() % nom.nbenfant]);
-                printf(" ");
-                printf("\n\n");
+                printf("Dans ce module vous pouvez choisir parmi les differents modeles\n");
+                printf("de generation de phrase proposes\n");
+                printf("Choix 1 : modele 1\nChoix 2 : modele 2\nChoix 3 : modele 3\nChoix : ");
+                scanf("%d", &choix1);
+                switch(choix1) {
+                    case 1 : {
+                        printf("\nphrase 1: ");
+                        __Nom_Aleatoire2(nom.child[rand() % nom.nbenfant]);
+                        printf(" ");
+                        __Adj_Aleatoire2(adj.child[rand() % adj.nbenfant]);
+                        printf(" ");
+                        __Verbe_Aleatoire2(vb.child[rand() % vb.nbenfant]);
+                        printf(" ");
+                        __Nom_Aleatoire2(nom.child[rand() % nom.nbenfant]);
+                        printf(" ");
+                        printf("\n\n");
+                        break;
+                    }
 
+                    case 2 : {
+                        printf("\nphrase 2: ");
+                        __Nom_Aleatoire2(nom.child[rand() % nom.nbenfant]);
+                        printf(" qui ");
+                        __Verbe_Aleatoire2(vb.child[rand() % vb.nbenfant]);
+                        printf(" ");
+                        __Verbe_Aleatoire2(vb.child[rand() % vb.nbenfant]);
+                        printf(" ");
+                        __Nom_Aleatoire2(nom.child[rand() % nom.nbenfant]);
+                        printf(" ");
+                        __Adj_Aleatoire2(adj.child[rand() % adj.nbenfant]);
+                        printf("\n\n");
+                        break;
+                    }
 
-                printf("phrase 2: ");
-                __Nom_Aleatoire2(nom.child[rand() % nom.nbenfant]);
-                printf(" qui ");
-                __Verbe_Aleatoire2(vb.child[rand() % vb.nbenfant]);
-                printf(" ");
-                __Verbe_Aleatoire2(vb.child[rand() % vb.nbenfant]);
-                printf(" ");
-                __Nom_Aleatoire2(nom.child[rand() % nom.nbenfant]);
-                printf(" ");
-                __Adj_Aleatoire2(adj.child[rand() % adj.nbenfant]);
-                printf("\n\n");
+                    case 3 : {
+                        printf("\nphrase 3: ");
+                        __Nom_Aleatoire2(nom.child[rand() % nom.nbenfant]);
+                        printf(" qui ");
+                        __Verbe_Aleatoire2(vb.child[rand() % vb.nbenfant]);
+                        printf(" ");
+                        __Verbe_Aleatoire2(vb.child[rand() % vb.nbenfant]);
+                        printf(" ");
+                        __Nom_Aleatoire2(nom.child[rand() % nom.nbenfant]);
+                        printf(" ");
+                        __Adj_Aleatoire2(adj.child[rand() % adj.nbenfant]);
+                        printf(" ");
+                        __Adv_Aleatoire2(adv.child[rand() % adv.nbenfant]);
+                        printf("\n\n");
+                        break;
+                    }
+                }
                 break;
             }
 
             case 2: {
-                char *no = "lait";
-                int nompresent = Nom_Present(no, nom);
-                printf("le nom %s est-il dans le dico ? %d\n", no, nompresent);
+                printf("Dans ce module, vous pourrez rechercher un mot dans le dictionnaire\n");
+                printf("Il sera possible d'afficher ses parametres\n");
+                printf("Choix 1 : Recherche de nom\nChoix 2 : Recherche de verbe\n");
+                printf("Choix 3 : Recherche d'adjectif\nChoix 4 : Recherche d'adverbe\nChoix : ");
+                scanf("%d", &choix2);
+                printf("\n");
+                switch(choix2)
+                {
+                    case 1: {
+                        char no[50];
+                        printf("Saissisez votre nom :");
+                        scanf("%s", no);
+                        int nompresent = Nom_Present(no, nom);
+                        if(nompresent == 1)
+                            res = "Oui";
+                        else
+                            res = "Non";
+                        printf("\nLe nom %s est-il dans le dico ? %s\n", no, res);
+                        break;
+                    }
 
-                char *no2 = "trotro";
-                int nompresent2 = Nom_Present(no2, nom);
-                printf("le nom %s est-il dans le dico ? %d\n", no2, nompresent2);
+                    case 2: {
+                        char ver[50];
+                        printf("Saissisez votre verbe :");
+                        scanf("%s", ver);
+                        int vbpresent = Verbe_Present(ver, vb);
+                        if(vbpresent == 1)
+                            res = "Oui";
+                        else
+                            res = "Non";
+                        printf("\nLe verbe %s est-il dans le dico ? %s\n", ver, res);
+                        break;
+                    }
+
+                    case 3: {
+                        char adjectif[50];
+                        printf("Saissisez votre adjectif :");
+                        scanf("%s", adjectif);
+                        int adjpresent = Adj_Present(adjectif, adj);
+                        if(adjpresent == 1)
+                            res = "Oui";
+                        else
+                            res = "Non";
+                        printf("\nL'adjectif %s est-il dans le dico ? %s\n", adjectif, res);
+                        break;
+                    }
+
+                    case 4: {
+                        char adverbe[50];
+                        printf("Saissisez votre adverbe :");
+                        scanf("%s", adverbe);
+                        int advpresent = Adv_Present(adverbe, adv);
+                        if(advpresent == 1)
+                            res = "Oui";
+                        else
+                            res = "Non";
+                        printf("\nL'adverbe %s est-il dans le dico? %s\n", adverbe, res);
+                        break;
+                    }
 
 
-                char *ver1 = "déplacer";
-                int vbpresent1 = Verbe_Present(ver1, vb);
-                printf("le verbe %s est-il dans le dico ? %d\n", ver1, vbpresent1);
 
-                char *ver = "abraquadabraquant";
-                int vbpresent = Verbe_Present(ver, vb);
-                printf("le verbe %s est-il dans le dico ? %d\n", ver, vbpresent);
-
-                char *adjectif = "fort";
-                int adjpresent = Adj_Present(adjectif, adj);
-                printf("le adjectif %s est-il dans le dico ? %d\n", adjectif, adjpresent);
-
-                char *adverbe = "lamentablement";
-                int advpresent = Adv_Present(adverbe, adv);
-                printf("le adverbe %s est-il dans le dico? %d\n", adverbe, advpresent);
+                }
                 break;
             }
 
